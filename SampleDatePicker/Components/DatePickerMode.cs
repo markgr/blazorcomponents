4b2e8972436bb354f3c5794568c8ec87d28347b9
Mark Greenwood <mark.greenwood@smokealarmsaustralia.com.au>
2024-08-19 20:32:11 +1000

    Updated code so that we can have date only, presets only etc

---
+++ SampleDatePicker/Components/DatePickerMode.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleDatePicker.Components
+{
+    public enum DatePickerMode
+    {
+        OnlyDate = 0,
+        PresetThenDate = 1,
+        PresetOnly = 2
+    }
+
+    public enum CurrentPickerMode
+    {
+        Calendar = 0,
+        Preset = 1
+    }
+}
